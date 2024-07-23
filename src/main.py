3d94f30a7a611430d655b0b42e55ec4d4c80e9e3
Gustavo Pavão <luisgustavopavao@hotmail.com>
2024-07-23 16:05:04 -0300

    Implemented the use via arguments

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import argparse
 from randomovie import RandoMovie
+from random import choice
 
-random = RandoMovie()
+randomovie = RandoMovie()
+parser = argparse.ArgumentParser(description="Random movie selector")
+parser.add_argument('usernames', type=str, nargs='+', help='Usernames to get the watchlist from')
 
+args = parser.parse_args()
+selected_username = choice(args.usernames)
 
-page = random.getting_page("pavaogus")
-watchlist = random.getting_watchlist(page)
-random.formatting_movies_names(watchlist)
-print(random.selected_movies)
+page = randomovie.getting_page(selected_username)
+watchlist = randomovie.getting_watchlist(page)
+randomovie.formatting_movies_names(watchlist)
+try:
+    print(str(choice(randomovie.selected_movies)).title())
+except:
+    print("There was an error! The username might be incorrect.")
